8a8bec5bdd35cef0cffe70f3b291f1b211affed6
ikemdinachi-alfred <ikemdinachialfred@gmail.com>
2024-06-11 18:58:47 +0100

    update

--- src/main/java/com/alfredTech/studentDetails/controllers/StudentController.java
+++ src/main/java/com/alfredTech/studentDetails/controllers/StudentController.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
+
 @CrossOrigin("http://localhost:3000/")
 @RestController
 @Controller
